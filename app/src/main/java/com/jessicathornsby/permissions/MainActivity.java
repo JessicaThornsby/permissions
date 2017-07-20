a72fc1314703d5a95cab56c857774489793b543a
JessicaThornsby <jessica.thornsby@gmail.com>
2017-07-20 20:00:14 +0100

    First commit
    First commit

---
+++ app/src/main/java/com/jessicathornsby/permissions/MainActivity.java
@@ -0,0 +1,76 @@
+package com.jessicathornsby.permissions;
+
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (checkPermission()) {
+                        Log.e("permission", "Permission already granted.");
+                    } else {
+                        requestPermission();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(MainActivity.this,
+                                "Permission accepted", Toast.LENGTH_LONG).show();
+                } else {
+                        Toast.makeText(MainActivity.this,
+                                "Permission denied", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+}
